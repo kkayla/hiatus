217408f717e0bc415519d7d84b559282098c3994
kkayla <kkayla.williams@icloud.com>
2016-03-02 16:58:59 -0500

    added spotify api with trending playlists on browse tab'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,13 @@ class HomeController < ApplicationController
   def index
     # @str = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=#{dest}&heading=151.78&pitch=-0.76&key=AIzaSyAL3tGbNJnkQPnEp5KRfIUi-TOJ6-LWZ0I"
   end
-
   def search
+    RSpotify::authenticate("ab28d4a1f6d843cbac32cca55f276abb", "22df4d4b2ccd4c8e948de0cb50c182e9")
+    @featured_playlists = RSpotify::Playlist.browse_featured(country: 'ES')
+    puts @featured_playlists
     @results= Rome2RioKayla.search(params[:coords],params[:query])
     # puts @results
-    render 'index', @results
+    render 'index'
+
   end
 end
